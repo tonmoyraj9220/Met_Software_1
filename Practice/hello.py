1ff0b0272c5f90c0b7d6e078f02ca22a4c2069eb
Tonmoy Rajbongshi <tonmoyraj9220@gmail.com>
2026-08-22 03:11:04 +0600

    Moveed to Practice Folder

---
+++ Practice/hello.py
@@ -0,0 +1,16 @@
+
+print("Hello World!")
+
+print("My Name is Tonmoy")
+
+print('"Hello", Said Tonmoy')
+print('"I\'m Tonmoy"')
+print("Good")
+print("Morning")
+print("Good\nMorning")
+
+
+name = input("Name:")
+print("Okay, you entered a name")
+
+print("Your name is " + name)
